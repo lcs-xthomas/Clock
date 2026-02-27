212d1e00117f646ba63b103ad6be6b7d5b37a108
Xavier Thomas-Evelyn <XThomasEvelyn@lcs.on.ca>
2026-02-27 09:21:09 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Zave T on 2026-02-27.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clock
+//
+//  Created by Zave T on 2026-02-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
